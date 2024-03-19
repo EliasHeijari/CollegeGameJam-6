28f438012b97c335432e73dad4c243078809c1ea
elkkuzq <69460338+EliasHeijari@users.noreply.github.com>
2024-03-19 20:56:11 +0200

    Hit effect when takes damage

--- Assets/EnemySystem/Player.cs
+++ Assets/EnemySystem/Player.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour, IDamageable
 {
     private int health = 100;
+    [SerializeField] private VolumeProfile hitEffectVolumeProfile;
+    [SerializeField] private Volume globalVolume;
+    private VolumeProfile oldVolumeProfile;
+
+    private void Start()
+    {
+        oldVolumeProfile = globalVolume.profile;
+    }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        StartCoroutine(HitEffectVolume());
         if (health <= 0)
         {
             health = 0;
             Die();
         }
+    }
+
+    IEnumerator HitEffectVolume()
+    {
+        globalVolume.profile = hitEffectVolumeProfile;
+        yield return new WaitForSeconds(2.5f);
+        globalVolume.profile = oldVolumeProfile;
     }
 
     private void Die()
